f1ae0760fcf8e46a93e91179d2bb87e4a7aa9f6c
Merlyn Hurtado <merlyn.hurtado@cri-paris.org>
2021-06-10 06:26:24 +0100

    Adding Time slider under the chart

--- .ipynb_checkpoints/covid_opendata-checkpoint.py
+++ .ipynb_checkpoints/covid_opendata-checkpoint.py
@@ -99,8 +99,9 @@ ca.update_layout(title="Daily Cases of Covid19",
                  xaxis = dict(title = 'Date'), 
                  yaxis = dict(title = 'Number of People'),
                  legend_title=dict(text='<b>Countries</b>'), 
-                 width = 800, height= 400, 
-                 plot_bgcolor= "white")
+                 width = 800, height= 500) 
+                 #plot_bgcolor= "white")
+ca.update_xaxes(rangeslider_visible=True)
                  
 #Death Chart 
 de = px.line( new_df, x = 'date', y = 'new_deaths', color = "location")
@@ -164,6 +165,7 @@ with col1:
         st.plotly_chart(de_pms, use_container_width=True)    
 
 with col2:   
+    st.title('''  ''')
     st.date_input('Select start date', datetime.date(2020, 1, 1))
     st.date_input('Select end date')
     normalization = st.checkbox("Relative to population")
