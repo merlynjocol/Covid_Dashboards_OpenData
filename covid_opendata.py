bb0ad866a0fa9b01b82ea64e29db53b3c9da6371
merlynjocol <45790494+merlynjocol@users.noreply.github.com>
2022-04-11 16:54:55 +0200

    Update covid_opendata.py

--- covid_opendata.py
+++ covid_opendata.py
@@ -6,7 +6,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 import warnings
-warnings.filterwarnings(‘ignore’)
+warnings.filterwarnings("ignore")
 
 #SETTTING THE PAGE TITLE AND ICON
 st.set_page_config(layout="wide", page_title= 'Covid Dashboards', page_icon="📈" )
